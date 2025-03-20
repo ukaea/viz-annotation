86eaa2818017f6c2ace1f75a7c829b35399d4733
Rui Costa <rorlandoc@gmail.com>
2025-03-20 12:05:37 +0000

    integrate plotly

--- services/ui/src/app/disruption/components/disruputionGraph.tsx
+++ services/ui/src/app/disruption/components/disruputionGraph.tsx
@@ -1,5 +1,6 @@
 'use client'
-import 'react-contexify/ReactContexify.css';
+import { use, useEffect } from 'react';
+import * as Plotly from "plotly.js";
 
 type GraphProps = {
 
@@ -12,9 +13,22 @@ type GraphProps = {
 }
 
 export const DisruptionGraph = ({ data, shot_id }: GraphProps) => {
+
+    useEffect(() => {
+        const plotElement: HTMLElement = document.getElementById('testPlot')!;
+
+        Plotly.newPlot(plotElement, [{
+            x: data.map(({ time }) => time),
+            y: data.map(({ value }) => value)
+        }], {
+            margin: { t: 0 }
+        });
+    });
+
     return (
         <div>
             <h2>Disruption ({shot_id})</h2>
+            <div id="testPlot"></div>
             <p style={{ whiteSpace: 'pre-line' }}>
                 {data.map(({ time, value }) => `time: ${time}, value: ${value}`).join('\n')}
             </p>

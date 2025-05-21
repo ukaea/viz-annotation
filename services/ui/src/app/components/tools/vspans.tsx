cfe0be682a4e1e0c76cd673b7bc82a75fc634347
Josh Blake <josh.blake@ukaea.uk>
2025-05-21 17:06:43 +0100

    Update vspan tool to auto populate subplots

--- services/ui/src/app/components/tools/vspans.tsx
+++ services/ui/src/app/components/tools/vspans.tsx
@@ -37,78 +37,103 @@ export const VSpans = ({plotId, plotReady} : VSpanProps) => {
 
         // Grab the handle set up in the main plot for D3 rendering
         const plot = document.getElementById(plotId)
-        const overplot = document.getElementsByClassName(`${plotId}-overplot`)[0]
 
         // Rendering should not be attempted if the required handles are not found
         if (!plot) {
             console.error("Could not locate plot to generate vspans")
             return
         }
-        if (!overplot) {
-            console.error("Could not locate D3 overplot to generate vspans")
-            return
-        }
 
-        // Finds the axis data which is used for scaling drawings - if subplots are used additional logic is needed
-        const marginTop = plot._fullLayout._size.t
-        const totalHeight = plot._fullLayout.yaxis._length
-        const y = marginTop - totalHeight
-        const height = 2 * totalHeight
+        // Get a reference to all subplots and find the name of the axis
+        const subplots = plot.querySelectorAll(".subplot")
+        const subplotNames = [...subplots].map(el => 
+        [...el.classList].find(cls => cls !== "subplot")
+        )
 
-        const xaxis = plot._fullLayout.xaxis;
+        // For each subplot carry out the tooling generation
+        subplotNames.forEach((subplotId => {
+            if (subplotId === undefined) {
+                console.error("Could not find valid subplot ID")
+                return
+            }
 
-        const graphGroup = d3.select(overplot)
-        graphGroup.selectAll(".vspan").remove() // All VSpans are removed each render cycle
+            const overplot = document.getElementsByClassName(`${plotId}-overplot-${subplotId}`)[0]
 
-        const drag = d3.drag<SVGRectElement, VSpan>()
-            .on("start", function (event, d) {
-                dragOffset.current = xaxis.d2p(d.x) - event.x
-            })
-            .on("drag", function (event, d) {
-                const newX = event.x + dragOffset.current;
-                d3.select(this).attr("x", newX);
+            if (!overplot) {
+                console.error("Could not locate D3 overplot to generate zones")
+                handleVSpanUpdate()
+                return
+            }
 
-                const x = xaxis.p2d(newX); // The context provider stores the decimal value rather than pixel
-                d.x = x;
-                handleVSpanUpdate() // Global refresh must be triggered to update all linked plots
-            })
+            // Find the y axis ID relating to this subplot
+            const yAxisID = subplotId.match(/y(.*)$/)?.[1];
+            if (!yAxisID && yAxisID !== "") {
+                console.error("Could not find valid subplot y-axis ID")
+                return
+            }
+            // Use the axis information to calculate the upper and lower limits of the zone
+            const axis = plot._fullLayout[`yaxis${yAxisID}`]
+            const limits = axis.range
+            const range = limits[1]-limits[0]
+            const upperLimit = axis.d2p(limits[1] + 2*range)
+            const lowerLimit =  axis.d2p(limits[0] - 2*range)
+            const height = lowerLimit - upperLimit
 
-        function handleContextMenu(event, vspan: VSpan) {
-            showVSpanMenu({
-                event,
-                props: {
-                    vspan
-                }
-            })
-        }
+            const xaxis = plot._fullLayout.xaxis;
 
-        // Create a line and a transparent drag handle for each VSpan
-        for (const vspan of vspans) {
-            const x = xaxis.d2p(vspan.x);
-            graphGroup.append("line")
-                .attr("class", "vspan")
-                .attr("x1", x)
-                .attr("x2", x)
-                .attr("y1", y)
-                .attr("y2", y + height)
-                .attr("stroke", vspan.category.color)
-                .attr("stroke-width", 6)
-                .attr("style", "pointer-events: all")
-                .style("cursor", "move")
+            const graphGroup = d3.select(overplot)
+            graphGroup.selectAll(".vspan").remove() // All VSpans are removed each render cycle
 
-            graphGroup.append("rect")
-                .attr("class", "vspan")
-                .attr("x", x-10)
-                .attr("y", y)
-                .attr("width", 20)
-                .attr("height", height)
-                .attr("fill", "transparent")
-                .attr("style", "pointer-events: all")
-                .style("cursor", "move")
-                .datum(vspan)
-                .call(drag)
-                .on("contextmenu", handleContextMenu)
-        }
+            const drag = d3.drag<SVGRectElement, VSpan>()
+                .on("start", function (event, d) {
+                    dragOffset.current = xaxis.d2p(d.x) - event.x
+                })
+                .on("drag", function (event, d) {
+                    const newX = event.x + dragOffset.current;
+                    d3.select(this).attr("x", newX);
+
+                    const x = xaxis.p2d(newX); // The context provider stores the decimal value rather than pixel
+                    d.x = x;
+                    handleVSpanUpdate() // Global refresh must be triggered to update all linked plots
+                })
+
+            function handleContextMenu(event, vspan: VSpan) {
+                showVSpanMenu({
+                    event,
+                    props: {
+                        vspan
+                    }
+                })
+            }
+
+            // Create a line and a transparent drag handle for each VSpan
+            for (const vspan of vspans) {
+                const x = xaxis.d2p(vspan.x);
+                graphGroup.append("line")
+                    .attr("class", "vspan")
+                    .attr("x1", x)
+                    .attr("x2", x)
+                    .attr("y1", upperLimit)
+                    .attr("y2", upperLimit + height)
+                    .attr("stroke", vspan.category.color)
+                    .attr("stroke-width", 6)
+                    .attr("style", "pointer-events: all")
+                    .style("cursor", "move")
+
+                graphGroup.append("rect")
+                    .attr("class", "vspan")
+                    .attr("x", x-10)
+                    .attr("y", upperLimit)
+                    .attr("width", 20)
+                    .attr("height", height)
+                    .attr("fill", "transparent")
+                    .attr("style", "pointer-events: all")
+                    .style("cursor", "move")
+                    .datum(vspan)
+                    .call(drag)
+                    .on("contextmenu", handleContextMenu)
+            }
+        }))
     }, [handleVSpanUpdate, plotId, plotReady, showVSpanMenu, vspans, triggerUpdate])
 
     return (
